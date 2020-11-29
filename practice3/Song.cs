02548259ffe1d8f9ec13fd60d6ffc770e131814d
redista <redista@users.github.com>
2020-11-29 20:42:02 +0000

    2 + 3

---
+++ practice3/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice3
+{
+    enum Genre
+    {
+        Rock,
+        Pop,
+        Dance,
+        Other
+    }
+
+    class Song
+    {
+    }
+}
